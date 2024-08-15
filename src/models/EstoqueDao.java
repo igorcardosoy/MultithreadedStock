daaa5f2fb492863296fb02e2ca7be516f047a639
Cauã Rufino de Sá <cauarufinodesa@gmail.com>
2024-08-14 22:01:49 -0300

    fix: Métodos da Interface sem modificadores.

--- src/models/EstoqueDao.java
+++ src/models/EstoqueDao.java
@@ -3,8 +3,8 @@ package models;
 import java.util.List;
 
 public interface EstoqueDao {
-    public Boolean adicionarProduto(Produto produto);
-    public Boolean removerProduto(Produto produto);
-    public List<Produto> buscarProdutos();
-    public Produto buscarProdutoPorId(int id);
+    Boolean adicionarProduto(Produto produto);
+    Boolean removerProduto(Produto produto);
+    List<Produto> buscarProdutos();
+    Produto buscarProdutoPorId(int id);
 }
