07ab3e454cb991b75b6eba17bf37747ee3786662
Cauã Rufino de Sá <cauarufinodesa@gmail.com>
2024-08-14 21:59:13 -0300

    feat: Implementação da interface EstoqueDao em EstoqueDaoImpl.

---
+++ src/models/EstoqueDaoImpl.java
@@ -0,0 +1,41 @@
+package models;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class EstoqueDaoImpl implements EstoqueDao {
+    private static EstoqueDaoImpl instance;
+    private Map<Integer, Produto> produtos;
+
+    private EstoqueDaoImpl() {
+        produtos = new HashMap<Integer, Produto>();
+    }
+
+    public static synchronized EstoqueDaoImpl getInstance() {
+        if (instance == null) {
+            instance = new EstoqueDaoImpl();
+        }
+        return instance;
+    }
+
+    @Override
+    public synchronized Boolean adicionarProduto(Produto produto) {
+        return produtos.put(produto.getId(), produto) == null;
+    }
+
+    @Override
+    public synchronized Boolean removerProduto(Produto produto) {
+        return produtos.remove(produto.getId()) != null;
+    }
+
+    @Override
+    public synchronized List<Produto> buscarProdutos() {
+        return produtos.values().stream().toList();
+    }
+
+    @Override
+    public synchronized Produto buscarProdutoPorId(int id) {
+        return produtos.get(id);
+    }
+}
